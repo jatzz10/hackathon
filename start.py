e45d7f3f2eea9b6a5468f68125a30e9b429d18d0
ayushpatidar <ayush.patidar4@gmail.com>
2019-08-03 20:43:16 +0530

    plot functionality added

--- start.py
+++ start.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, render_template
-
+from plot_function import city
 app = Flask(__name__)
 
-@app.route('/admin')
+@app.route('/admin', methods = ["POST", "GET"])
 def admin():
-  return render_template('index.html')
 
+    return render_template('index.html')
 @app.route('/user')
 def index():
   return render_template('index.html')
