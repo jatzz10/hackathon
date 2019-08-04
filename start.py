8e2af6eb200453a03c5320845b4965cfc8594a3a
Jatin Sharma <jatin.sharma@fyle.in>
2019-08-04 07:33:35 +0530

    Add functionalities

--- start.py
+++ start.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, render_template
-from plot_function import city
+# from plot_function import city
 
 app = Flask(__name__)
+
+@app.route('/home')
+def home():
+  return render_template('index.html')
 
 @app.route('/admin')
 def admin():
   return render_template('index.html')
 
 @app.route('/user')
-def index():
+def user():
   return render_template('index.html')
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,42 +1,87 @@
-window.onload = function() {
+function displayChart(d1, d2, chartType, chartTitle, y) {
   var dataPoints = [];
-
-  var chart = new CanvasJS.Chart("chartContainer", {
+  var chart = new CanvasJS.Chart('chartContainer', {
     animationEnabled: true,
-    theme: "light2",
+    theme: 'light2',
     title: {
-      text: "Daily Sales Data"
+      text: chartTitle
     },
     axisY: {
-      title: "Units",
+      title: y,
       titleFontSize: 24
     },
     data: [{
-      type: "column",
-      yValueFormatString: "#,### Units",
+      type: chartType,
+      yValueFormatString: '#,### Units',
       dataPoints: dataPoints
     }]
   });
 
-  function addData(data) {
-    alert('hello');
-    for (var i = 0; i < data.x.length; i++) {
-      dataPoints.push({
-        x: data.x[i],
-        y: data.y[i]
-      });
-      alert(dataPoints);
-    }
-    chart.render();
+  for (var i = 0; i < d1.length; i++) {
+    dataPoints.push({
+      x: d1[i],
+      y: d2[i]
+    });
   }
-
-  $.getJSON("data.json", addData);
-};
+  chart.render();
+}
 
 $(function() {
+  if (window.location.pathname === '/admin') {
+    $('.admin').show();
+    $('.user').hide();
+    $('.home').hide();
+  }
+  else if (window.location.pathname === '/user') {
+    $('.admin').hide();
+    $('.user').show();
+    $('.home').hide();
+  }
+  else {
+    $('.home').show();
+    $('.admin').hide();
+    $('.user').hide();
+  }
+
+  $('.thor-left').on('click', function () {
+    window.location.href = '/admin';
+  });
+
+  $('.hulk-right').on('click', function () {
+    window.location.href = '/user';
+  });
+
   $('select').change(function() {
     var result = $('.insights-result');
-    var value = $(".insights-class option:selected").val();
-    result.text(value);
-  });
+    var value = $('.insights-class option:selected').val();
+    if (value === '1') {
+      var d1 = [1120112, 1120224, 1120560, 1120336, 1120672, 1120784, 1120448];
+      var d2 = [4877866, 2647335, 854047, 732380, 300091, 250875, 16385];
+      displayChart(d1,d2, 'pie', 'City having highest transactions, y');
+    }
+    if (value === '2') {
+      $('#chartContainer').html('<img class="size" src="/static/img/category.png">')
+    }
+    if (value === '3') {
+      $('#chartContainer').html('<img class="size" src="/static/img/city3.png">')
+    }
+    if (value === '4') {
+      $('#chartContainer').html('<img class="size" src="/static/img/heatmap.png">')
+    }
+    if (value === '5') {
+      $('#chartContainer').html('<img class="size" src="/static/img/hour_trans1.png">')
+    }
+    if (value === '6') {
+      $('#chartContainer').html('<img class="size" src="/static/img/month.png">')
+    }
+    if (value === '7') {
+      $('#chartContainer').html('<img class="size" src="/static/img/month_trend.png">')
+    }
+    if (value === '8') {
+      $('#chartContainer').html('<img class="size" src="/static/img/month_trend_sub.png">')
+    }
+    if (value === '9') {
+      $('#chartContainer').html('<img class="size" src="/static/img/product.png">')
+    }
+  }).change();
 });
